251f1cb828890be1099233db41ab83519083c4ad
HonestWildCat <honestwildcat@gmail.com>
2022-11-15 14:49:52 +0200

    V 0.3.3

--- to_array.py
+++ to_array.py
@@ -11,7 +11,7 @@ with open('categories/berries.txt', 'r', encoding='utf-8') as f:
     line = f.readline()
     while line != "@":
         arr.append(line[:-1])
-        print(f'{line.lower()}', end="")
+        # print(f'{line.lower()}', end="")
         line = f.readline()
 # with open('categories/clothing.txt', 'r', encoding='utf-8') as f:
 #     line = f.readline()
@@ -23,7 +23,7 @@ with open('categories/countries.txt', 'r', encoding='utf-8') as f:
     line = f.readline()
     while line != "@":
         arr.append(line[:-1])
-        print(f'{line.lower()}', end="")
+        # print(f'{line.lower()}', end="")
         line = f.readline()
 # with open('categories/dishes.txt', 'r', encoding='utf-8') as f:
 #     line = f.readline()
@@ -35,25 +35,25 @@ with open('categories/flowers.txt', 'r', encoding='utf-8') as f:
     line = f.readline()
     while line != "@":
         arr.append(line[:-1])
-        print(f'{line.lower()}', end="")
+        # print(f'{line.lower()}', end="")
         line = f.readline()
 with open('categories/fruits.txt', 'r', encoding='utf-8') as f:
     line = f.readline()
     while line != "@":
         arr.append(line[:-1])
-        print(f'{line.lower()}', end="")
+        # print(f'{line.lower()}', end="")
         line = f.readline()
 with open('categories/furniture.txt', 'r', encoding='utf-8') as f:
     line = f.readline()
     while line != "@":
         arr.append(line[:-1])
-        print(f'{line.lower()}', end="")
+        # print(f'{line.lower()}', end="")
         line = f.readline()
 with open('categories/herbs and spices.txt', 'r', encoding='utf-8') as f:
     line = f.readline()
     while line != "@":
         arr.append(line[:-1])
-        print(f'{line.lower()}', end="")
+        # print(f'{line.lower()}', end="")
         line = f.readline()
 # with open('categories/professions.txt', 'r', encoding='utf-8') as f:
 #     line = f.readline()
@@ -65,13 +65,15 @@ with open('categories/trees.txt', 'r', encoding='utf-8') as f:
     line = f.readline()
     while line != "@":
         arr.append(line[:-1])
-        print(f'{line.lower()}', end="")
+        # print(f'{line.lower()}', end="")
         line = f.readline()
 with open('categories/vegetables.txt', 'r', encoding='utf-8') as f:
     line = f.readline()
     while line != "@":
         arr.append(line[:-1])
-        print(f'{line.lower()}', end="")
+        # print(f'{line.lower()}', end="")
         line = f.readline()
 
 print(arr)
+with open('dictionary_edited.txt', 'w') as f:
+    f.write(", ".join(arr))
