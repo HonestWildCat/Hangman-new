3dfafa9afb527c9efb2458938692da337d1618cb
HonestWildCat <honestwildcat@gmail.com>
2024-02-26 21:16:01 +0200

    Delete json_converter.py

--- json_converter.py
+++
@@ -1,9 +0,0 @@
-import json, codecs
-
-with codecs.open('russian_nouns_with_definition.json', encoding='utf-8') as f:
-    data = json.load(f)
-key = data.keys()
-with open('dictionary.txt', 'w') as f:
-    for i in key:
-        print(f'{[i, "", data[i]["definition"]]},\n')
-        f.write(f'{[i, "", data[i]["definition"]]},\n')
